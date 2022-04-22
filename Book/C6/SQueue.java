57c39be99f24eaa74f895e2fe144890a6c14870b
root <root@LAPTOP-NDKRDD79.localdomain>
2022-04-23 00:39:10 +0800

    C6

---
+++ Book/C6/SQueue.java
@@ -0,0 +1,23 @@
+/** Implements Queue based on stacks.
+ *  @author Vingo
+ */
+import java.util.Stack;
+public class Queue<T> {
+    private T item;
+    private int size;
+
+    public Queue() {
+        item = null;
+        size = 0;
+    }
+
+    /** Adds an element to the end of the queue. */
+    public void push() {
+
+    }
+
+    /** Pops the element of the front of the queue. */
+    public T poll() {
+        return null;
+    }
+}
---
+++ Book/C6/vingo/dog/Dog.java
@@ -0,0 +1,2 @@
+package vingo.dog;public class Dog {
+}
